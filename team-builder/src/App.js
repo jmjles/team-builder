08ba1c19f7390567c46de48beb88648d003a591b
jmjles <jmjles@hotmail.com>
2019-11-13 17:14:58 -0800

    MVP Complete | Cleaned up code

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,32 +1,24 @@
-import React,{useState} from "react";
-import Form from './components/form';
-import {Container} from '@material-ui/core';
+import React, { useState } from "react";
+import Form from "./components/form";
+import { Container } from "@material-ui/core";
 
-import Person from './components/person'
-const id = require('uniqid');
+import Person from "./components/person";
+const id = require("uniqid");
 function App() {
-
-  const [list,updateList] = useState([
+  const [list, updateList] = useState([
     {
-      id:id(),
-      name:'John',
-      title:'Frontend Web Dev',
-      comment:'Fun guy to work with!'
-    },
-    {
-      id:id(),
-      name:'Ethan',
-      title:'Frontend Web Dev',
-      comment:'Fun guy to work with!'
+      id: id(),
+      name: "Some Random Guy",
+      title: "Frontend Web Dev",
+      comment: "Fun guy to work with!"
     }
   ]);
-  return(
-    <Container lg ='true'>
-      <Form list={list} fn={updateList}/>
-      <Person list={list}/>
+  return (
+    <Container lg="true">
+      <Form list={list} fn={updateList} id={id} />
+      <Person list={list} />
     </Container>
-    
   );
-};
+}
 
 export default App;
--- team-builder/src/components/person.js
+++ team-builder/src/components/person.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import Card from './card';
-import {Paper, Container} from '@material-ui/core';
-const Person = ({list})  => {
-    console.log(list)
-    return(
-        <Container>
-            <Paper sm>
-                {
-                    list.map(({id,name,title,comment}) => (
-                        <Card 
-                            key={id}
-                            name={name}
-                            title={title}
-                            comment={comment}
-                        />
-                    ))
-                }
-            </Paper>
-        </Container>
-        
-    );
+import React from "react";
+import Card from "./card";
+import { Paper, Container } from "@material-ui/core";
+
+const Person = ({ list }) => {
+  return (
+    <Container>
+      <Paper elevation={5}>
+        {list.map(({ id, name, title, comment }) => (
+          <Card key={id} name={name} title={title} comment={comment} />
+        ))}
+      </Paper>
+    </Container>
+  );
 };
 
 export default Person;
